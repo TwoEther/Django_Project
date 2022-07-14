b56952e134b643921021fff8892c1e97bda5a676
TwoEther <lhi0711@naver.com>
2022-07-14 14:27:30 +0900

    포스트 상세 페이지 테스트 하기

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,4 @@
+import os.path
 from distutils.command.upload import upload
 from django.db import models
 
@@ -6,6 +7,7 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
+    hook_text = models.CharField(max_length=100, blank=True)
 
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d', blank=True)
     file_upload = models.FileField(upload_to='blog/files/%Y/%m/%d/', blank=True)
@@ -17,5 +19,12 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+
+    def get_file_exit(self):
+        return self.get_file_name().split('.')[-1]
+
     # auto. 추후 작성
 #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
